5b67807e0875dcbf8734b3aba3b63fc780699406
vthuan.dev <165972755+vthuan-dev@users.noreply.github.com>
2024-12-24 15:11:30 +0700

    fix: update CORS configuration

--- server/server.js
+++ server/server.js
@@ -7,7 +7,8 @@ import connectDatabase from "./src/config/connectDB";
 const app = express();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: ['https://le-phong-tro.onrender.com', 'http://localhost:3000'],
+    credentials: true,
     methods: ["POST", "GET", "PUT", "DELETE"],
   })
 );

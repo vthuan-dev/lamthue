5ccab69364f5ead2796bcecd58ae9ef950bab0ca
vthuan.dev <165972755+vthuan-dev@users.noreply.github.com>
2024-12-24 15:02:39 +0700

    fix fwe

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -31,7 +31,7 @@ module.exports = {
           "slide-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both",
       },
       keyframes: {
-        " slide-right ": {
+        "slide-right": {
           "0%": {
             "-webkit-transform": "translateX(0)",
             transform: "translateX(0)",
